f779a27f6774379570ae6f6b93e0796dd93ca308
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-04-13 00:23:35 -0700

    Add a sleep statement before m5 exit
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- rv64gc-hpc/gem5-init.sh
+++ rv64gc-hpc/gem5-init.sh
@@ -6,14 +6,15 @@ if ! [[ "$IGNORE_M5" == 1 ]]; then
     # if it returns 0, it means there's no inputted command
     if m5 readfile > script.sh; then
         chmod +x script.sh;
-        echo "Executing the following commands\n"
+        echo "Executing the following commands"
         echo "//------"
         cat script.sh
         echo "//------"
         ./script.sh
+        sleep 1
         m5 exit
     else
-        echo "No inputted command. Dropping to bash.\n"
+        echo "No inputted command. Dropping to bash."
         IGNORE_M5=1 /bin/bash
     fi
 fi
