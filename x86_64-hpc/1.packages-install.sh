d4f70f8d73601fca6868699281a84b90570d3709
Hoa Nguyen <hn@hnpl.org>
2023-09-27 15:32:42 -0700

    Add libomp-dev dependency
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- x86_64-hpc/1.packages-install.sh
+++ x86_64-hpc/1.packages-install.sh
@@ -3,7 +3,7 @@
 # Installing the dependencies
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install build-essential git python3-pip gfortran cmake ninja-build git-lfs
+sudo apt-get -y install build-essential git python3-pip gfortran cmake ninja-build git-lfs libomp-dev
 pip install scons --user
 pip install meson --user
 
