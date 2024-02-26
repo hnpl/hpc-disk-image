f70365e16a8a426b669ab7b77faad6434ab6d36d
Hoa Nguyen <hn@hnpl.org>
2024-02-26 21:57:03 +0000

    Add NUMA dependency
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- x86_64-hpc/1.packages-install.sh
+++ x86_64-hpc/1.packages-install.sh
@@ -4,6 +4,9 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install build-essential git python3-pip gfortran cmake ninja-build git-lfs libomp-dev
+# install numa library
+sudo apt-get -y install numactl libnuma-dev libnuma1
+
 pip install scons --user
 pip install meson --user
 
