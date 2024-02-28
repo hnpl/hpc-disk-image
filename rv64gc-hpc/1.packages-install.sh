4f967637e04a90ab23cc48d3c491afde77651e88
Hoa Nguyen <hn@hnpl.org>
2024-02-28 14:40:56 -0800

    Update 1.packages-install.sh
    libomp-dev is not available for riscv

--- rv64gc-hpc/1.packages-install.sh
+++ rv64gc-hpc/1.packages-install.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install build-essential git python3-pip gfortran cmake ninja-build git-lfs libomp-dev
+sudo apt-get -y install build-essential git python3-pip gfortran cmake ninja-build git-lfs
 pip install scons --user
 pip install meson --user
